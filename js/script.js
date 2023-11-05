4a202f6b80a66928f7c7d829ebcd408da9f712da
ykamran16 <ykamran@hawk.iit.edu>
2023-11-05 11:59:50 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-window.onload = function(){
-  const button = document.querySelector('input[type="submit"]');
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
 
-  button.addEventListener("click", myFunction);
+// function for username  digit requirement
+
+var form = document.getElementById('form');
+
+// alert for form completion
+function alertFunction() {
+  if (form.checkValidity()) {
+    alert("You have successfully signed up\nCheck your email for more information");
+  }

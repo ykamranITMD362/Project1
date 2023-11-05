54526669a80bcdb3eec8d06d4bae87e5879442c4
ykamran16 <ykamran@hawk.iit.edu>
2023-11-05 12:16:08 -0600

    Chnaged get to post in the form action

--- js/script.js
+++ js/script.js
@@ -3,12 +3,11 @@ jQuery(function($) {
   $('html').addClass('hasjs');
 });
 
-// function for username  digit requirement
 
 var form = document.getElementById('form');
 
 // alert for form completion
 function alertFunction() {
   if (form.checkValidity()) {
-    alert("You have successfully signed up\nCheck your email for more information");
+    alert("You have now signed up!");
   }
